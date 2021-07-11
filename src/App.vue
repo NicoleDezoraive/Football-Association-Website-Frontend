0c5395bf2a007b9f07f40656e0104499b0436ea9
NicoleDezoraive <49281896+NicoleDezoraive@users.noreply.github.com>
2021-07-11 21:49:50 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <!-- <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand :to="{ name: 'main' }">Superliga Vue</b-navbar-brand>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+
+        <b-nav-item :to="{ name: 'search' }">Search</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto" v-if="!$root.store.username">
+          <b-nav-item :to="{ name: 'login' }">Login</b-nav-item>
+          <b-nav-item :to="{ name: 'register' }">Register</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto" v-else>
+        <b-nav-item-dropdown right>
+          <template #button-content>
+            User
+          </template>
+          <b-dropdown-item href="#">Favorites</b-dropdown-item>
+          <b-dropdown-item href="#">Log Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar> -->
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import NavBar from "./components/NavBar"
+  export default {
+    name: "App",
+    components: {
+      NavBar,
+    },
+  };
+</script> 
+
+<style lang="scss">
+  @import "@/scss/form-style.scss";
+  #app {
+    font-family: Tahoma, Geneva, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  #app::before {
+    content: "";
+    display: block;
+    position: absolute;
+    min-height: 100%;
+    min-width: 1024px;
+    width: 100%;
+    height: auto;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: url("https://img.playbuzz.com/image/upload/ar_1.8867924528301887,c_crop/q_auto:good,f_auto,fl_lossy,w_640,c_limit,dpr_1/v1537461819/sfbrtk5qop9k1uqq0jlp.jpg");
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    opacity: 0.5;
+    z-index: -1;
+  }
+</style>
